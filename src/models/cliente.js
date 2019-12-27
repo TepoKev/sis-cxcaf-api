844cb4bd2618187c3b4275a2f11c6ddf0419e2c6
TepoKev <tepokev@gmail.com>
2019-12-26 22:06:31 -0600

    Se agrego el modelo de la tabla cliente

---
+++ src/models/cliente.js
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+import {sequelize} from "../database/database";
+import Persona from "../models/persona";
+
+const Cliente = sequelize.define('cliente', {
+  id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  tipo: {
+    type: Sequelize.ENUM('Persona Natural','Persona Jurídica'),
+    allowNull: false
+  },
+  idPersona: {
+    type: Sequelize.INTEGER(11),
+    allowNull: true,
+    references: {
+      model: 'persona',
+      key: 'id'
+    }
+  },
+  clasificacion: {
+    type: Sequelize.STRING(1),
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  tableName: 'cliente'
+});
+
+Cliente.belongsTo(Persona, {foreignKey: 'idPersona'});
+
+export default Cliente;
