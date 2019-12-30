9dd325276073d99764ab2c036c0d9df4d91fbcfb
TepoKev <tepokev@gmail.com>
2019-12-29 20:56:10 -0600

    Se agrego el metodo Update

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -64,7 +64,7 @@ export async function createCliente( req, res ){
             direccion,
             zona,
             estadoCivil,
-            //Datos de usuario
+            //Datos de cliente
             tipo,
             clasificacion
         } = req.body;
@@ -95,10 +95,113 @@ export async function createCliente( req, res ){
         newPersona.dataValues.profesion = profesion.dataValues;
         newCliente.dataValues.persona = newPersona.dataValues;
         res.json(newCliente);
+        res.status(200);
     } catch (error) {
         console.log(error);
         res.status(500).json({
             message : "Error en el servidor"
+        });
+    }
+}
+
+export async function updateCliente( req, res ){
+    try {
+        var opc = false;
+        const { id } = req.params;
+        const {
+            //Datos de persona
+            nombres,
+            apellidos,
+            dui,
+            telefono,
+            genero,
+            salario,
+            idProfesion,
+            nit,
+            fechaN,
+            direccion,
+            zona,
+            estadoCivil,
+            //Datos de usuario
+            tipo,
+            clasificacion
+        } = req.body;
+        const clientes = await Cliente.findAll({
+            attributes : [
+                "id",
+                "tipo",
+                "idPersona",
+                "clasificacion"
+            ],
+            where : {
+                id
+            }
+        });
+        if(clientes.length > 0 ){
+            clientes.forEach(async cliente => {
+                await cliente.update({
+                    tipo,
+                    clasificacion
+                });
+                const personas = await Persona.findAll({
+                    attributes : [
+                        "id",
+                        "nombres",
+                        "apellidos",
+                        "dui",
+                        "telefono",
+                        "genero",
+                        "salario",
+                        "idProfesion",
+                        "nit",
+                        "fechaN",
+                        "direccion",
+                        "zona",
+                        "estadoCivil",
+                    ],
+                    where : {
+                        id : cliente.dataValues.idPersona
+                    }
+                });
+                if(personas.length > 0 ){
+                    personas.forEach(async persona => {
+                        await persona.update({
+                            nombres,
+                            apellidos,
+                            dui,
+                            telefono,
+                            genero,
+                            salario,
+                            idProfesion,
+                            nit,
+                            fechaN,
+                            direccion,
+                            zona,
+                            estadoCivil,
+                        });
+                    });
+                    opc = false;
+                }
+            });
+            if(opc){
+                return res.json({
+                    message: "Cliente actualizado exitosamente",
+                    data: clientes
+                });
+            }else{
+                return res.json({
+                    message: "Cliente y Persona actualizados exitosamente",
+                    data: clientes
+                });
+            }
+        }else{
+            res.status(400).json({
+                message: "Alguno de los registros no se ha encontrado"
+            });    
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Somethin goes wrong"
         });
     }
 }
