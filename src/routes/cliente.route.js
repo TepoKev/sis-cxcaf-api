598fc1bc7c7c4cf466c2cee7000eab18036d675c
TepoKev <tepokev@gmail.com>
2019-12-29 20:57:13 -0600

    Se agrego la ruta para el metodo update

--- src/routes/cliente.route.js
+++ src/routes/cliente.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getClientes, getOneCliente, createCliente } from "../controllers/cliente.controller";
+import { getClientes, getOneCliente, createCliente, updateCliente } from "../controllers/cliente.controller";
 
 const router = Router();
 
 router.get("/", getClientes);
 router.get("/:id", getOneCliente);
 router.post("/", createCliente);
+router.put("/:id", updateCliente);
 
 export default router;
