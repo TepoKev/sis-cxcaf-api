986489d7123d1a110349567f95b618444563a52c
TepoKev <tepokev@gmail.com>
2019-12-30 20:22:42 -0600

    Se agrego la relacion entre Cliente y Usuario

--- src/models/cliente.js
+++ src/models/cliente.js
@@ -1,13 +1,14 @@
 import Sequelize from "sequelize";
 import { sequelize } from "../database/database";
 import Persona from "./persona";
+import Usuario from "./usuario";
 
 const Cliente = sequelize.define('cliente', {
   id: {
     type: Sequelize.INTEGER(11),
     allowNull: false,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement : true
   },
   tipo: {
     type: Sequelize.ENUM('Persona Natural','Persona Jurídica'),
@@ -19,17 +20,28 @@ const Cliente = sequelize.define('cliente', {
     references: {
       model: 'persona',
       key: 'id'
-    }
+    },
+    unique: true
   },
   clasificacion: {
     type: Sequelize.STRING(1),
     allowNull: false
+  },
+  idUsuario: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'usuario',
+      key: 'id'
+    },
+    unique: true
   }
 }, {
   timestamps: false,
   tableName: 'cliente'
 });
 
-Cliente.belongsTo(Persona, {foreignKey: 'idPersona'});
+Cliente.belongsTo( Persona, { foreignKey: 'idPersona' } );
+Cliente.belongsTo( Usuario, { foreignKey : 'idUsuario' } );
 
 export default Cliente;
