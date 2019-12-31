f2b4437c5ebe1dfba5050968f7eb4f18a530211d
TepoKev <tepokev@gmail.com>
2019-12-30 20:33:59 -0600

    Se agrego el modelo Credito

---
+++ src/models/credito.js
@@ -0,0 +1,67 @@
+import Sequelize from "sequelize";
+import { sequelize } from "../database/database";
+import Politica from "./politica";
+import Cliente from "./cliente";
+import Empleado from "./empleado";
+
+const Credito = sequelize.define('credito', {
+  id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement : true
+  },
+  monto: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  fecha: {
+    type: Sequelize.DATEONLY,
+    allowNull: false
+  },
+  cobro: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  idEmpleado: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'empleado',
+      key: 'id'
+    }
+  },
+  idPolitica: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'politica',
+      key: 'id'
+    }
+  },
+  estado: {
+    type: Sequelize.ENUM('cancelado','en cobro','incobrable'),
+    allowNull: false
+  },
+  idCliente: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'cliente',
+      key: 'id'
+    }
+  },
+  tipoCredito: {
+    type: Sequelize.ENUM('PERSONAL','HIPOTECARIO','NOMINA','DE INVERSION','DE VIVIENDA','DE CONSUMO','DE ESTUDIO','DE AVIO','AGRARIO'),
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  tableName: 'credito'
+});
+
+Credito.belongsTo(Politica , { fireignKey : "idPolitica" });
+Credito.belongsTo(Cliente , { foreignKey : "idCliente" });
+Credito.belongsTo(Empleado , { foreignKey : "idEmpleado" });
+
+export default Credito;
