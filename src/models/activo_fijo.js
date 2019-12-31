90bd4bdf5a8e7079dbae4be2f61ed90fb0293a5d
TepoKev <tepokev@gmail.com>
2019-12-30 20:30:38 -0600

    Se agrego el modelo Activo Fijo

---
+++ src/models/activo_fijo.js
@@ -0,0 +1,87 @@
+import Sequelize from "sequelize";
+import { sequelize } from "../database/database";
+import Empleado from "./empleado";
+import Departamento from "./departamento";
+import Marca from "./marca";
+import Sucursal from "./sucursal";
+import TipoActivo from "./tipo_activo";
+
+const ActivoFijo = sequelize.define('activo_fijo', {
+  id: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement : true
+  },
+  nombre: {
+    type: Sequelize.STRING(60),
+    allowNull: false
+  },
+  idEmpleado: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'empleado',
+      key: 'id'
+    }
+  },
+  descripcion: {
+    type: Sequelize.STRING(300),
+    allowNull: false
+  },
+  procedencia: {
+    type: Sequelize.ENUM('NUEVO','USADO','DONADO NUEVO','DONADO USADO'),
+    allowNull: false
+  },
+  precio: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  idDepartamento: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'departamento',
+      key: 'id'
+    }
+  },
+  idTipo: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'tipo_activo',
+      key: 'id'
+    }
+  },
+  idMarca: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'marca',
+      key: 'id'
+    }
+  },
+  idSucursal: {
+    type: Sequelize.INTEGER(11),
+    allowNull: false,
+    references: {
+      model: 'sucursal',
+      key: 'id'
+    }
+  },
+  fechaAdquisicion: {
+    type: Sequelize.DATEONLY,
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  tableName: 'activo_fijo'
+});
+
+ActivoFijo.belongsTo( Empleado, { foreignKey : "idEmpleado" } );
+ActivoFijo.belongsTo( Departamento, { foreignKey : "idDepartamento" } );
+ActivoFijo.belongsTo( Marca, { foreignKey : "idMarca" } );
+ActivoFijo.belongsTo( Sucursal, { foreignKey : "idSucursal" } );
+ActivoFijo.belongsTo( TipoActivo, { foreignKey : "idTipo" } );
+
+export default ActivoFijo ;
